9ab409304d1cfea3618bcdd56a611cf54dda66f9
rahulgubili <rahul.gubiligari@paypay-corp.co.jp>
2024-06-15 09:41:06 +0900

    Add topup function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -9,7 +10,9 @@ import (
 	"github.com/rahulgubili3003/digital-wallet/model"
 	"gorm.io/gorm"
 	"log"
+	"net/http"
 	"os"
+	"time"
 )
 
 type Repository struct {
@@ -45,7 +48,7 @@ func (r *Repository) CreateWallet(ctx *fiber.Ctx) error {
 
 	if err != nil {
 		err := ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
-			"message": "Could not create Book"})
+			"message": "Could not create Wallet"})
 		if err != nil {
 			return err
 		}
@@ -59,8 +62,66 @@ func (r *Repository) CreateWallet(ctx *fiber.Ctx) error {
 	return nil
 }
 
+type TopUpRequest struct {
+	UserId      uint    `json:"user_id"`
+	TopUpAmount float64 `json:"top_up_amount"`
+}
+
 func (r *Repository) TopUp(ctx *fiber.Ctx) error {
-	return nil
+
+	topUp := TopUpRequest{}
+
+	if err := ctx.BodyParser(&topUp); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"message": "Failed to Parse the Request"})
+	}
+
+	wallet, err := r.findWalletByUserId(topUp.UserId)
+
+	if err != nil {
+		err := ctx.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"message": "Could not find Wallet"})
+		if err != nil {
+			return err
+		}
+	}
+
+	existingBal := wallet.Balance
+	newBal := existingBal + topUp.TopUpAmount
+
+	result := r.DB.Model(&wallet).Where("wallet_id =? AND user_id =?", wallet.WalletId, wallet.UserId).Updates(map[string]interface{}{
+		"balance":    newBal,
+		"updated_at": time.Now(),
+	})
+
+	if result.Error != nil {
+		// Handle error
+		return ctx.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Failed to update wallet balance",
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(&fiber.Map{
+		"message":   "Top up successful",
+		"wallet_id": wallet.WalletId,
+		"balance":   wallet.Balance})
+}
+
+func (r *Repository) findWalletByUserId(userId uint) (*model.Wallet, error) {
+	var wallet model.Wallet
+	result := r.DB.Where("user_id =?", userId).First(&wallet)
+
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			// No wallet found for the given user_id
+			return nil, fmt.Errorf("no wallet found for user_id %d", userId)
+		} else {
+			// Some other error occurred
+			return nil, result.Error
+		}
+	}
+
+	return &wallet, nil
 }
 
 func (r *Repository) setupRoutes(app *fiber.App) {
